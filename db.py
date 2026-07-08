07c78d99dfa5d7b69d28750ee5903934ba1be54d
Nanu Boro <dibiasboro1@gmail.com>
2026-07-08 21:10:05 +0530

    Connect Flask app to TiDB cloud database

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
 import mysql.connector
 
 def get_db_connection():
-    print("Password =", repr("Nova@Cloud7Sun"))
-
     return mysql.connector.connect(
         host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+        
         user="3CtSNVW5UEePkaK.root",
-        password="70TFN2qVnkl909PF",
-        database="sys"
+        password="cn5hmISK8UCGm4qD",
+        database="School",
+        port=4000,
+        ssl_verify_cert=True,
+        ssl_ca="isrgrootx1.pem"
     )
