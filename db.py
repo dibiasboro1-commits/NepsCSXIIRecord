d04590ef3b1de98d20fc8f9d34aaebc213792214
Nanu Boro <dibiasboro1@gmail.com>
2026-07-08 20:43:06 +0530

    connect TiDB cloud database

--- db.py
+++ db.py
@@ -4,8 +4,8 @@ def get_db_connection():
     print("Password =", repr("Nova@Cloud7Sun"))
 
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Nova@Cloud7Sun",
-        database="School"
+        host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+        user="3CtSNVW5UEePkaK.root",
+        password="70TFN2qVnkl909PF",
+        database="sys"
     )
